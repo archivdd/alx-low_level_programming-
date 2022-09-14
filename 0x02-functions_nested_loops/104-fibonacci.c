926a54be2967d063b72b4c1aeb14c2596ceeab36
root <root@DESKTOP-K1MQ2FD>
2022-09-15 01:03:16 +0200

    104=Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-int main() {
-  int t1 = 0, t2 = 1, nextTerm = 0, n;
-  printf("Enter a positive number: ");
-  scanf("%d", &n);
-
-  // displays the first two terms which is always 0 and 1
-  printf("Fibonacci Series: %d, %d, ", t1, t2);
-  nextTerm = t1 + t2;
-
-  while (nextTerm <= n) {
-    printf("%d, ", nextTerm);
-    t1 = t2;
-    t2 = nextTerm;
-    nextTerm = t1 + t2;
-  }
-
-  return 0;
+ 
+// Function to print
+// first n Fibonacci Numbers
+void printFibonacciNumbers(int n)
+{
+    int f1 = 0, f2 = 1, i;
+ 
+    if (n < 98)
+        return;
+    printf("%d ", f1);
+    for (i = 1; i < n; i++) {
+        printf("%d ", f2);
+        int next = f1 + f2;
+        f1 = f2;
+        f2 = next;
+{
+     return 0;
 }
